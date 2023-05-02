91eff70659126f620914d0ee61763db034b86ebf
Alberto <42894888+Nenino2@users.noreply.github.com>
2023-05-02 20:48:51 +0200

    feat(init): blast off

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# xotoscript-action-submodules
+# Xotoscript action submodules
+
+### Summary
+
+When working with a repository that has multiple submodules, it can be difficult to keep track of changes and pipelines across all submodules. 
+
+This can lead to inefficiencies and errors in the development process. To address this problem, we have created a custom GitHub action that automatically creates a bot comment in the root pull request, listing all pull requests of submodules, including their status and pipeline.
+
+### Description
+
+To use this action, follow these steps:
+
+1. Add a job with this action in your workflow file:
+
+```yaml
+custom-action:
+  runs-on: ubuntu-latest
+  container:
+    image: node:16
+    options: --user root
+  steps:
+    - name: Checkout root repo
+      uses: actions/checkout@v3
+      with:
+        repository: xotoscript/xotoscript-action-submodules
+        token: ${{ secrets.GH_PAT }}
+    - name: Sync submodules changelog
+      uses: ./
+      with:
+        ci_token: ${{ secrets.GITHUB_TOKEN }}
+        root_token: ${{ secrets.GH_PAT }}
+        target_owner: xotoscript
+        target_submodules: |
+          xotoscript-submodule-1
+          xotoscript-submodule-2
+        target_branch: ${{ github.head_ref || github.ref_name }}
+        target_workflow: ci-workflow.yml
+```
+
+1. In the job, specify the required inputs:
+- **`ci_token`**: a GitHub token for the CI.
+- **`root_token`**: a personal access token with the **`repo`** scope to access all repos of the organization.
+- **`target_owner`**: the organization (xotoscript).
+- **`target_submodules`**: a list of submodules in the repository.
+- **`target_branch`**: the name of the branch of the current pull request.
+- **`target_workflow`**: the name of the workflow file to use.
+1. Run the workflow.
+
+The action will automatically create a bot comment in the root pull request, listing all pull requests of submodules, including their status and pipeline.
+
+### Conclusion
+
+With our custom GitHub action, you can easily keep track of changes and pipelines across all submodules in your repository, reducing inefficiencies and errors in the development process. Use this action in your workflow to improve your development process today.
---
+++ src/main.ts
@@ -0,0 +1,99 @@
+import * as core from '@actions/core'
+import * as github from "@actions/github"
+import { Octokit, } from "octokit";
+
+async function getTargetPrData(targetOwner: string, targetRepo: string, targetBranch: string) {
+  const api = new Octokit({
+    auth: core.getInput('root_token'),
+  });
+
+  // Get target pull request's data
+  const { data } = await api.rest.pulls.list({
+    owner: targetOwner,
+    repo: targetRepo,
+    head: targetOwner + ":" + targetBranch,
+    sort: "updated",
+    state: "all"
+  })
+
+  if (data.length === 0) return null;
+  const prData = data[0]
+  return prData;
+}
+
+async function removePreviousBotComment() {
+  const api = new Octokit({
+    auth: core.getInput('ci_token'),
+  });
+
+  const { data } = await api.rest.issues.listComments({
+    owner: github.context.issue.owner,
+    repo: github.context.issue.repo,
+    issue_number: github.context.issue.number
+  })
+
+  let comment_id;
+
+  for (let comment of data) {
+    if (comment.body.includes("xotoscript action info")) {
+      comment_id = comment.id;
+      break;
+    }
+  }
+
+  if (comment_id === undefined) return;
+
+  await api.rest.issues.deleteComment({
+    owner: github.context.issue.owner,
+    repo: github.context.issue.repo,
+    comment_id
+  })
+}
+
+async function pushCommentOnPr(targetPrDataList: {
+  title: string;
+  html_url: string;
+  state: string;
+  owner: string;
+  repo: string;
+  branch: string;
+}[]) {
+  const api = new Octokit({
+    auth: core.getInput('ci_token'),
+  });
+
+  let body = `# xotoscript action info \n`
+
+  for (let targetPrData of targetPrDataList) {
+    body += `## [${targetPrData.repo} - ${targetPrData.title}](${targetPrData.html_url})\n**pull request status**: ${targetPrData.state}\n**pipeline status**: ![worflow](https://github.com/${targetPrData.owner}/${targetPrData.repo}/actions/workflows/${core.getInput('target_workflow')}/badge.svg?branch=${targetPrData.branch})\n`
+  }
+
+  await api.rest.issues.createComment({
+    owner: github.context.issue.owner,
+    repo: github.context.issue.repo,
+    issue_number: github.context.issue.number,
+    body
+  })
+}
+
+async function run(): Promise<void> {
+  try {
+    const owner = core.getInput('target_owner');
+    const submodules = core.getMultilineInput("target_submodules")
+    const targetBranch = core.getInput("target_branch")
+    const targetPrDataList = []
+
+    for (let submodule of submodules) {
+      const targetPrData = await getTargetPrData(owner, submodule, targetBranch)
+      if (targetPrData) targetPrDataList.push({ ...targetPrData, owner: owner, repo: submodule, branch: targetBranch })
+    }
+
+    await removePreviousBotComment()
+    await pushCommentOnPr(targetPrDataList)
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run()
